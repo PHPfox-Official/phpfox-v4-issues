4c5bb8c0faec89f685dc08b4b23571d85a1682be
Raymond Benc <r@phpfox.com>
2015-03-13 03:23:47 +0100

    save

--- upload/module/core/template/default/block/template-breadcrumblist.html.php
+++ upload/module/core/template/default/block/template-breadcrumblist.html.php
@@ -15,16 +15,9 @@ defined('PHPFOX') or exit('NO DICE!');
 		{if $iBreadTotal = count($aBreadCrumbs)}{/if}
 		<div id="breadcrumb_list">
 			<ul>
-				{if Phpfox::isMobile()}
-				<li><a href="#" onclick="window.history.back(); return false;" class="mobile_back">{phrase var='core.back'}</a></li>
-				{else}
-				<li><a href="{url link=''}" class="home" title="{phrase var='core.home'}">{phrase var='core.home'}</a></li>
-				{/if}
 			{foreach from=$aBreadCrumbs key=sLink item=sCrumb name=link}
 				<li>{if !empty($sLink)}<a href="{$sLink}" class="ajax_link{if $phpfox.iteration.link == '1'} first{/if}">{/if}{$sCrumb|clean}{if !empty($sLink)}</a>{/if}</li>
-				{if $iBreadTotal != $phpfox.iteration.link}<li>&raquo;</li>{/if}
 			{/foreach}
 			</ul>
-			<div class="clear"></div>
 		</div>
 		{/if}

33fb691ebd0e628dd630fae3dc9606e7d22b8bf3
Raymond Benc <r@phpfox.com>
2015-04-18 11:45:30 +0200

    Updated URL class to start the replacement of old PF lib.

--- PF.Src/Core/Url.php
+++ PF.Src/Core/Url.php
@@ -2,6 +2,38 @@
 
 namespace Core;
 
-class Url extends \Phpfox_Url {
+/**
+ * Class Url
+ * @package Core
+ *
+ * @method make($route)
+ * @method send($route)
+ * @method uri()
+ */
+class Url {
 
+	private static $_url = null;
+
+	public function __construct() {
+		if (self::$_url === null) {
+			self::$_url = \Phpfox_Url::instance();
+		}
+	}
+
+	public function route() {
+		return '/' . trim(str_replace('.', '/', \Phpfox_Module::instance()->getFullControllerName()), '/');
+	}
+
+	public function __call($method, $args) {
+		switch ($method) {
+			case 'make':
+				$method = 'makeUrl';
+				break;
+			case 'uri':
+				$method = 'getUri';
+				break;
+		}
+
+		return call_user_func_array([self::$_url, $method], $args);
+	}
 }

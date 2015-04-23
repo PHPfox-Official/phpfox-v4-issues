95c924def0e14ba2072eb860f98c68dff6420374
Raymond Benc <r@phpfox.com>
2015-04-23 09:04:13 +0200

    Exporting MD5 checksum and saving RC2 XML files.

---
+++ PF.Base/module/forum/install/version/4.0.0rc2.xml.php
@@ -0,0 +1,3 @@
+<upgrade>
+	<sql><![CDATA[a:1:{s:9:"ADD_FIELD";a:2:{s:17:"phpfox_forum_post";a:1:{s:10:"cache_name";a:4:{i:0;s:9:"VCHAR:250";i:1;N;i:2;s:0:"";i:3;s:3:"YES";}}s:19:"phpfox_forum_thread";a:1:{s:10:"cache_name";a:4:{i:0;s:9:"VCHAR:250";i:1;N;i:2;s:0:"";i:3;s:3:"YES";}}}}]]></sql>
+</upgrade>

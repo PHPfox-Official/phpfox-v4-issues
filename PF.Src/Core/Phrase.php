d85d76c5d194c65a6c58d20afc74a797bffa4f07
Raymond Benc <r@phpfox.com>
2015-07-11 21:26:27 +0200

    fix https://github.com/moxi9/phpfox/issues/56

--- PF.Src/Core/Phrase.php
+++ PF.Src/Core/Phrase.php
@@ -8,7 +8,16 @@ class Phrase {
 		$phrase = $args[0];
 		unset($args[0]);
 
-		$key = substr(preg_replace('/[^a-z0-9]+/', '_', strtolower($phrase)), 0, 250);
+		// $key = substr(preg_replace('/[^a-z0-9]+/', '_', strtolower($phrase)), 0, 250);
+		if (preg_match('/^([a-z]+)\.([a-zA-Z0-9_]+)$/', $phrase)) {
+			return \Phpfox::getPhrase($phrase);
+		}
+
+		if (isset($args[1]) && count($args[1])) {
+			foreach ($args[1] as $key => $value) {
+				$phrase = str_replace('{{ ' . $key . ' }}', $value, $phrase);
+			}
+		}
 
 		return $phrase;
 	}

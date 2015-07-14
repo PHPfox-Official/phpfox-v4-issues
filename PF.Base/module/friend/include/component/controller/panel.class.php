2c2ac4ea6ce7201334188cda158d2cf3e35b1b7a
Raymond Benc <r@phpfox.com>
2015-07-14 15:47:42 +0200

    Updating the default friend request limit. It was way too low.

--- PF.Base/module/friend/include/component/controller/panel.class.php
+++ PF.Base/module/friend/include/component/controller/panel.class.php
@@ -4,7 +4,7 @@ class Friend_Component_Controller_Panel extends Phpfox_Component {
 	public function process() {
 		Phpfox::isUser(true);
 
-		list($iCnt, $aFriends) = Phpfox::getService('friend.request')->get();
+		list($iCnt, $aFriends) = Friend_Service_Request_Request::instance()->get(0, 100);
 		$this->template()->assign([
 			'aFriends' => $aFriends
 		]);
